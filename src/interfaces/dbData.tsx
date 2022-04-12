f4426b11ee6dc606e6c47838077d9cf6c38a6233
cherrycoke2l <maciekwojcik1@gmail.com>
2022-04-12 17:22:22 +0200

    Changed type formating: removed spaces before colons

--- src/interfaces/dbData.tsx
+++ src/interfaces/dbData.tsx
@@ -1,41 +1,41 @@
 export interface User {
-  id : string;
-  name : string;
-  surname : string;
-  login : string;
-  password : string;
-  email : string;
-  score : number;
-  lastActivity : Date;
-  groups : string[];
-  ownedGroups : string[];
-  activities : string[];
+  id: string;
+  name: string;
+  surname: string;
+  login: string;
+  password: string;
+  email: string;
+  score: number;
+  lastActivity: Date;
+  groups: string[];
+  ownedGroups: string[];
+  activities: string[];
 }
 
 export interface Group {
-  id : string;
-  name : string;
-  description : string;
-  members : string[];
-  owner : string;
+  id: string;
+  name: string;
+  description: string;
+  members: string[];
+  owner: string;
 }
 
 export interface Activity {
-  id : string;
-  exercise : string;
-  date : Date;
-  duration : string;
-  score : number;
+  id: string;
+  exercise: string;
+  date: Date;
+  duration: string;
+  score: number;
 }
 
 export interface Exercise {
-  id : string;
-  name : string;
-  weight : number;
+  id: string;
+  name: string;
+  weight: number;
 }
 
 export interface Invitation {
-  id : string;
-  group : string;
-  user : string;
+  id: string;
+  group: string;
+  user: string;
 }
