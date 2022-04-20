1b8dc4fddf4377b2fce79b7050b2e13356fdf363
Magdalena Kita <magdalenakita88@gmail.com>
2022-04-20 23:11:12 +0200

    Created header component with logo, photo profile and log out

---
+++ src/components/Header/style.ts
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+`;
+
+export const Img = styled.img`
+  height: 100px;
+  align-self: center;
+`;
+
+export const RightSection = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightButton = styled.div`
+  cursor: pointer;
+  margin-left: 20px;
+`;
+
+export const Logout = styled.span`
+  font-size: 18px;
+  font-family: ${(props) => props.theme.fontFamily.primaryFont};
+  font-weight: bold;
+  color: black;
+`;
+
+export const PhotoProfile = styled.div`
+  width: 40px;
+  height: 40px;
+  border: 2px solid ${(props) => props.theme.colors.secondaryUi};
+  border-radius: 50%;
+  cursor: pointer;
+  background-image: url("assets/background_photo.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
