4a92ffca05a435868f7cd5f909ef72c3341fdb34
natimati <natmateusiak@gmail.com>
2022-04-23 12:28:03 +0200

    adding group owner as group member

--- src/services/groupService.ts
+++ src/services/groupService.ts
@@ -1,26 +1,11 @@
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
-import { Group } from "../interfaces/dbData";
+import { Group, User } from "../interfaces/dbData";
 
-export const createGroup = async (group: Omit<Group, "uid">, onSuccess: () => void) => {
-  try {
-    await addDoc(collection(getFirestore(), "groups"), group);
-    onSuccess();
-  } catch (err: unknown) {
-    console.log(err);
-    if (err instanceof Error) {
-      alert(err.message);
-    }
-  }
+export const createGroup = async (group: Omit<Group, "uid">) => {
+  await addDoc(collection(getFirestore(), "groups"), group);
 };
 
-export const getUserIds = async () => {
-  try {
-    const response = await getDocs(collection(getFirestore(), "users"));
-    return response?.docs.map((doc) => doc.data().login as string);
-  } catch (err: unknown) {
-    console.log(err);
-    if (err instanceof Error) {
-      alert(err.message);
-    }
-  }
+export const getUsers = async () => {
+  const response = await getDocs(collection(getFirestore(), "users"));
+  return response?.docs.map((doc) => doc.data() as User);
 };
