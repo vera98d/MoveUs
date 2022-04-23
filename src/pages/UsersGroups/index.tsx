731fac160bde1a4fa0d8ee10fb8fd8be264173d9
natimati <natmateusiak@gmail.com>
2022-04-23 20:57:52 +0200

    handling navigation

--- src/pages/UsersGroups/index.tsx
+++ src/pages/UsersGroups/index.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import BackgroundContainer from "../../components/BackgroundContainer/styles";
 import { Header } from "../../components/Typography/styles";
 import { Tile, TileContainer, TileContent } from "./styles";
 import { groups, user } from "./mockedData";
+import { ModalContext } from "../../context/ModalContextProvider";
+import GroupForm from "../../components/GroupForm";
 
 const UsersGroups = (): JSX.Element => {
   const usersGroups: (JSX.Element | undefined)[] = groups.map((group) => {
     if (group.owner === user.uid) {
       return (
-        <Link to={`/groups/${group.uid}`} key={group.uid}>
+        <Link to={`/team-jo-project-4/groups/${group.uid}`} key={group.uid}>
           <Tile img={group.imageUrl}>
             <TileContent>
               {group.name}
@@ -23,7 +25,7 @@ const UsersGroups = (): JSX.Element => {
   const groupsUserBelongsTo: (JSX.Element | undefined)[] = groups.map((group) => {
     if (group.uid in user.groups && group.owner !== user.uid) {
       return (
-        <Link to={`/groups/${group.uid}`} key={group.uid}>
+        <Link to={`/team-jo-project-4/groups/${group.uid}`} key={group.uid}>
           <Tile img={group.imageUrl}>
             <TileContent>
               {group.name}
@@ -34,11 +36,13 @@ const UsersGroups = (): JSX.Element => {
     }
   });
 
+  const { setDisplayedComponent } = useContext(ModalContext);
+
   return (
     <BackgroundContainer>
       <Header>My groups</Header>
       <TileContainer>
-        <Tile>
+        <Tile onClick={() => setDisplayedComponent(<GroupForm />)}>
           <TileContent>
             Create new group
           </TileContent>
