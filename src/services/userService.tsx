3dc8f6c2c4928de9846d87ac6db74df681b97ea7
cherrycoke2l <maciekwojcik1@gmail.com>
2022-04-24 02:04:48 +0200

    fixed getUSer

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -10,18 +10,18 @@ class UserService {
     const q = query(collection(this.db, "users"), where("uid", "==", userId));
     const querySnapshot = await getDocs(q);
 
-    return querySnapshot.docs.map((doc) => ({
-      uid: doc.id,
-      name: doc.data().name,
-      surname: doc.data().surname,
-      login: doc.data().login,
-      password: doc.data().password,
-      email: doc.data().email,
-      score: doc.data().score,
-      lastActivity: doc.data().lastActivity,
-      groups: doc.data().groups,
-      ownedGroups: doc.data().ownedGroups,
-      activities: doc.data().activities,
+    return querySnapshot.docs.map((docu) => ({
+      uid: docu.id,
+      name: docu.data().name,
+      surname: docu.data().surname,
+      login: docu.data().login,
+      password: docu.data().password,
+      email: docu.data().email,
+      score: docu.data().score,
+      lastActivity: docu.data().lastActivity,
+      groups: docu.data().groups,
+      ownedGroups: docu.data().ownedGroups,
+      activities: docu.data().activities,
     }))[0];
   };
 
