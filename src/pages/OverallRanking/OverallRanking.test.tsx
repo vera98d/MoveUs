9c2f282b5c3cbf47a3f7291e042c628e79fa8c9f
Magdalena Kita <magdalenakita88@gmail.com>
2022-04-24 23:45:12 +0200

    Added overall ranking unit test

---
+++ src/pages/OverallRanking/OverallRanking.test.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import {BrowserRouter as Router} from 'react-router-dom';
+import { ThemeProvider } from "styled-components";
+import OverallRanking from ".";
+import userService from "../../services/userService";
+import { theme } from "../../styles";
+import { users } from "./mock";
+
+let orginalGetAll: any;
+beforeEach(() => {
+  orginalGetAll = userService.getAllUsers;
+  userService.getAllUsers = () => Promise.resolve(users);
+});
+
+afterEach(() => {
+    userService.getAllUsers = orginalGetAll;
+});
+
+describe("OverallRanking", () => {
+    test("matches snapshot", async () => {
+      const { container } = render(
+        <ThemeProvider theme={theme}>
+          <Router>
+            <OverallRanking />
+          </Router>
+        </ThemeProvider>
+      );
+      expect(container).toMatchSnapshot();
+    });
+
+    test("is rendering all users", async () => {
+        render(
+            <ThemeProvider theme={theme}>
+              <Router>
+                <OverallRanking />
+              </Router>
+            </ThemeProvider>
+          );
+        expect(users.length).toBe(users.length);
+    });
+
+    test('Header should show "Overall Ranking"', () => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+                <OverallRanking />
+              </Router>
+          </ThemeProvider>
+        );    
+        const header = screen.queryByText("Overall Ranking");
+        expect(header).toBeInTheDocument();
+      });
+});
+  
