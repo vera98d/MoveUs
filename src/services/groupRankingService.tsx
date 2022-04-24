bb02d549453b067dd059e6d391b43f2a0b4b5028
Weronika Luteńko <vera98d@gmail.com>
2022-04-24 22:33:14 +0200

    fixed usergroupservice to not throw error if user does not belong to any group

--- src/services/groupRankingService.tsx
+++ src/services/groupRankingService.tsx
@@ -4,41 +4,47 @@ import { User } from "../interfaces/dbData";
 class GroupRankingService {
   getGroupsUserBelongsTo = async (currentUserInfo: User) => {
     const groupRef = collection(getFirestore(), "groups");
-    const groupNumbersUsersBelongsTo = currentUserInfo.groups.filter((group: string) => {
-      return !currentUserInfo.ownedGroups.includes(group);
-    });
-    const queryUsersGroups = query(
-      groupRef,
-      where(documentId(), "in", groupNumbersUsersBelongsTo),
-    );
-    const usersGroupsSnapshot = await getDocs(queryUsersGroups);
+    if (currentUserInfo.groups.length > 0) {
+      const groupNumbersUsersBelongsTo = currentUserInfo.groups.filter((group: string) => {
+        return !currentUserInfo.ownedGroups.includes(group);
+      });
+      const queryUsersGroups = query(
+        groupRef,
+        where(documentId(), "in", groupNumbersUsersBelongsTo),
+      );
+      const usersGroupsSnapshot = await getDocs(queryUsersGroups);
 
-    return usersGroupsSnapshot.docs.map((doc) => ({
-      uid: doc.id,
-      name: doc.data().name,
-      description: doc.data().description,
-      members: doc.data().members,
-      owner: doc.data().owner,
-      imageUrl: doc.data().imageUrl,
-    }));
+      return usersGroupsSnapshot.docs.map((doc) => ({
+        uid: doc.id,
+        name: doc.data().name,
+        description: doc.data().description,
+        members: doc.data().members,
+        owner: doc.data().owner,
+        imageUrl: doc.data().imageUrl,
+      }));
+    }
+    return [];
   };
 
   getUsersOwnedGroups = async (currentUserInfo: User) => {
     const groupRef = collection(getFirestore(), "groups");
-    const queryUsersGroups = query(
-      groupRef,
-      where(documentId(), "in", currentUserInfo.ownedGroups),
-    );
-    const usersGroupsSnapshot = await getDocs(queryUsersGroups);
+    if (currentUserInfo.ownedGroups.length > 0) {
+      const queryUsersGroups = query(
+        groupRef,
+        where(documentId(), "in", currentUserInfo.ownedGroups),
+      );
+      const usersGroupsSnapshot = await getDocs(queryUsersGroups);
 
-    return usersGroupsSnapshot.docs.map((doc) => ({
-      uid: doc.id,
-      name: doc.data().name,
-      description: doc.data().description,
-      members: doc.data().members,
-      owner: doc.data().owner,
-      imageUrl: doc.data().imageUrl,
-    }));
+      return usersGroupsSnapshot.docs.map((doc) => ({
+        uid: doc.id,
+        name: doc.data().name,
+        description: doc.data().description,
+        members: doc.data().members,
+        owner: doc.data().owner,
+        imageUrl: doc.data().imageUrl,
+      }));
+    }
+    return [];
   };
 
   getGroupMembersInfo = async (groupId: string) => {
@@ -50,23 +56,26 @@ class GroupRankingService {
     const ByGroupIdSnapshot = await getDocs(queryByGroupId);
     const groupInfo = ByGroupIdSnapshot.docs[0].data();
 
-    const userRef = collection(getFirestore(), "users");
-    const queryByGroupMembers = query(userRef, where("uid", "in", groupInfo.members));
-    const querySnapshot = await getDocs(queryByGroupMembers);
+    if (groupInfo.members.length > 0) {
+      const userRef = collection(getFirestore(), "users");
+      const queryByGroupMembers = query(userRef, where("uid", "in", groupInfo.members));
+      const querySnapshot = await getDocs(queryByGroupMembers);
 
-    return querySnapshot.docs.map((doc) => ({
-      uid: doc.data().uid,
-      name: doc.data().name,
-      surname: doc.data().surname,
-      login: doc.data().login,
-      password: doc.data().password,
-      email: doc.data().email,
-      score: doc.data().score,
-      lastActivity: doc.data().lastActivity,
-      groups: doc.data().groups,
-      ownedGroups: doc.data().ownedGroups,
-      activities: doc.data().activities,
-    }));
+      return querySnapshot.docs.map((doc) => ({
+        uid: doc.data().uid,
+        name: doc.data().name,
+        surname: doc.data().surname,
+        login: doc.data().login,
+        password: doc.data().password,
+        email: doc.data().email,
+        score: doc.data().score,
+        lastActivity: doc.data().lastActivity,
+        groups: doc.data().groups,
+        ownedGroups: doc.data().ownedGroups,
+        activities: doc.data().activities,
+      }));
+    }
+    return [];
   };
 
   getGroupInfoById = async (groupId: string) => {
