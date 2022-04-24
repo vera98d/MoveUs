1146ca2eab3255ea061804dcf8e96069ca8e25af
cherrycoke2l <maciekwojcik1@gmail.com>
2022-04-24 18:34:45 +0200

    added randomized exercises method

--- src/pages/Workout/index.tsx
+++ src/pages/Workout/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Exercise, H3, Tiles } from "./style";
+import exerciseService from "../../services/exerciseService";
 
 function Workout() {
+  exerciseService.getRandomExercises(0);
   return (
     <Container>
       <Tiles>
--- src/services/exerciseService.tsx
+++ src/services/exerciseService.tsx
@@ -1,4 +1,5 @@
 import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { Exercise } from "../interfaces/dbData";
 
 class ExerciseService {
   getAll = async () => {
@@ -6,6 +7,20 @@ class ExerciseService {
     const querySnapshot = await getDocs(docRef);
     return querySnapshot.docs.map((doc) => ({ ...doc.data() }));
   };
-}
 
+  getRandomExercises = async (number: number) => {
+    function shuffleArray(array: Exercise[]) {
+      const newArray = array;
+      for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [array[j], array[i]];
+      }
+      return newArray;
+    }
+    const array = this.getAll().then((data) => {
+      console.log(data);
+      // const newArray = shuffleArray(data);
+    });
+  };
+}
 export default new ExerciseService();
