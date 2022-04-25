e2091c2875313c246de71c6e44cd105bf71629cf
Magdalena Kita <magdalenakita88@gmail.com>
2022-04-25 14:43:46 +0200

    Modified home test

--- src/pages/Home/home.test.tsx
+++ src/pages/Home/home.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import {BrowserRouter as Router} from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
-import HomePage from ".";
+import HomePage from "./index";
 import { theme } from "../../styles";
 import { waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
 
 describe("HomePage", () => {
     test("matches snapshot", async () => {
@@ -18,65 +19,75 @@ describe("HomePage", () => {
     });
 
     test('First tile should show "Overall ranking"', () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <Router>
-            <HomePage />
-          </Router>
-        </ThemeProvider>
-      );
+      act(() => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+              <HomePage />
+            </Router>
+          </ThemeProvider>
+        );
+      });
   
       const tileOne = screen.queryByText("Overall ranking");
       waitFor(() => expect(expect(tileOne).toBeInTheDocument()));
     });
 
     test('Second tile should show "Groups"', () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <Router>
-            <HomePage />
-          </Router>
-        </ThemeProvider>
-      );
+      act(() => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+              <HomePage />
+            </Router>
+          </ThemeProvider>
+        );
+      });
   
       const tileTwo = screen.queryByText("Groups");
       waitFor(() => expect(expect(tileTwo).toBeInTheDocument()));
     });
 
     test('Third tile should show "Activity"', () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <Router>
-            <HomePage />
-          </Router>
-        </ThemeProvider>
-      );
+      act(() => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+              <HomePage />
+            </Router>
+          </ThemeProvider>
+        );
+      });
   
       const tileThree = screen.queryByText("Activity");
       waitFor(() => expect(expect(tileThree).toBeInTheDocument()));
     });
 
     test('Fourth tile should show "Workout"', () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <Router>
-            <HomePage />
-          </Router>
-        </ThemeProvider>
-      );
+      act(() => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+              <HomePage />
+            </Router>
+          </ThemeProvider>
+        );
+      });
   
       const tileFour = screen.queryByText("Workout");
       waitFor(() => expect(expect(tileFour).toBeInTheDocument()));
     });
 
     test("includes images", () => {
-      render(
-        <ThemeProvider theme={theme}>
-          <Router>
-            <HomePage />
-          </Router>
-        </ThemeProvider>
-      );
+      act(() => {
+        render(
+          <ThemeProvider theme={theme}>
+            <Router>
+              <HomePage />
+            </Router>
+          </ThemeProvider>
+        );
+      });
   
       const image = screen.findAllByRole("img");
       waitFor(() => expect(image).toBeInTheDocument());
