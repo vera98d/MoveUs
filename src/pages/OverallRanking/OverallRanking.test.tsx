6e24c1a8084298ee670ca9b3ff37dc83c145a5b2
Magdalena Kita <magdalenakita88@gmail.com>
2022-04-25 16:23:47 +0200

    Added type of originalGetAll. Changed second test

--- src/pages/OverallRanking/OverallRanking.test.tsx
+++ src/pages/OverallRanking/OverallRanking.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import {BrowserRouter as Router} from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import OverallRanking from ".";
+import { User } from "../../interfaces/dbData";
 import userService from "../../services/userService";
 import { theme } from "../../styles";
 import { users } from "./mock";
@@ -14,14 +15,14 @@ jest.mock("react-firebase-hooks/auth", () => ({
   useAuthState: () => ([])
 }));
 
-let orginalGetAll: any;
+let orginalGetAll: () => Promise<User[]>;
 beforeEach(() => {
   orginalGetAll = userService.getAllUsers;
   userService.getAllUsers = () => Promise.resolve(users);
 });
 
 afterEach(() => {
-    userService.getAllUsers = orginalGetAll;
+  userService.getAllUsers = orginalGetAll;
 });
 
 describe("OverallRanking", () => {
@@ -36,27 +37,27 @@ describe("OverallRanking", () => {
       expect(container).toMatchSnapshot();
     });
 
-    test("is rendering all users", async () => {
-        render(
-            <ThemeProvider theme={theme}>
-              <Router>
-                <OverallRanking />
-              </Router>
-            </ThemeProvider>
-          );
-        expect(users.length).toBe(users.length);
+    test("is rendering all 6 users", async () => {
+      render(
+        <ThemeProvider theme={theme}>
+          <Router>
+            <OverallRanking />
+          </Router>
+        </ThemeProvider>
+      );
+      expect(users.length).toBe(6);
     });
 
     test('Header should show "Overall Ranking"', () => {
-        render(
-          <ThemeProvider theme={theme}>
-            <Router>
-                <OverallRanking />
-              </Router>
-          </ThemeProvider>
-        );    
-        const header = screen.queryByText("Overall Ranking");
-        expect(header).toBeInTheDocument();
-      });
+      render(
+        <ThemeProvider theme={theme}>
+          <Router>
+            <OverallRanking />
+          </Router>
+        </ThemeProvider>
+      );    
+      const header = screen.queryByText("Overall Ranking");
+      expect(header).toBeInTheDocument();
+    });
 });
   
